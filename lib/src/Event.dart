184d6c26c0b4724bd864bdb4d4f4ae937acc819a
epere82917 <erik.perez.pm@gmail.com>
2015-07-24 16:55:55 -0400

    Stubbed Event class

---
+++ lib/src/Event.dart
@@ -0,0 +1,44 @@
+/**
+ * Event.dart
+ * Friend computer 2.0
+ * copyright 2015 Pioneer Valley Gamer Collective
+ */
+
+/**
+ * Event
+ * This class is used to create Event objects and methods for Event objects.
+ */
+
+class Event {
+  int _id;
+  int _parentid;
+
+  Event () {
+
+  }
+
+  List <int> getParties () {
+    //TODO: Implement getParties in Event
+    List <int> parties = new List<int>();
+    return parties;
+  }
+
+  List <int> getSchedules () {
+    //TODO: Implement getSchedules in Event
+    List <int> schedules = new List <int>;
+    return schedules;
+  }
+
+  void addParty () {
+    //TODO: Implement addParty in Event
+  }
+
+  void removeParty () {
+    //TODO: Implement removeParty in Event
+  }
+
+  void addSchedule () {
+    //TODO: Implement addSchedule for Event
+  }
+
+}
