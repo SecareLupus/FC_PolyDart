4bd8c6f0501568a1f14190a6cae3e071ffe87f9f
Desmond Duval <haydre@gmail.com>
2015-08-27 11:20:57 -0400

    Changing server listening process. Attempting bugfix.

--- serve/lib/FCApi.dart
+++ serve/lib/FCApi.dart
@@ -1,24 +1,23 @@
 import "package:rpc/rpc.dart";
-import "package:redis_client/redis_client.dart";
-import "MessageForms.dart";
-import "KeyGen.dart";
+import "MessageForms.dart" as Message;
+import "KeyGen.dart" as KeyGen;
 
 @ApiClass(
   name: 'fc',
   version: 'v1',
   description: 'Friend Computer API')
 class FCApi {
-  @ApiMethod(method: 'GET', path: 'helo/{pubkey}')
-  SynResponse getLoginKey(String pubkey) {
+  @ApiMethod(method: 'GET', path: 'syn')
+  Message.SynResponse getLoginKey() {
     print("getLoginKey() accessed");
-    var tmp = KeyGen.getLoginKey("test");
+    var tmp = KeyGen.KeyGen.getLoginKey("test");
     print("Key: $tmp");
-    print(KeyGen.getPerms(tmp.login_key));
+    print(KeyGen.KeyGen.getPerms(tmp.login_key));
     return null;
   }
 
   @ApiMethod(method: 'POST', path: 'resource/{name}/update')
-  VoidMessage updateResource(String name, MSG2 request) {
-    return;
+  VoidMessage updateResource(String name, Message.MSG2 request) {
+    return new VoidMessage();
   }
 }
