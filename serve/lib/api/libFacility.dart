618d39ca3a3474a48305a6316f1e23090bea4812
Desmond Duval <haydre@gmail.com>
2016-04-22 08:16:48 -0400

    Mapped out libFacility because it looked easy. Now I'm going to get pancakes and see if I can implement this after.

--- serve/lib/api/libFacility.dart
+++ serve/lib/api/libFacility.dart
@@ -7,41 +7,35 @@ export "dbObject.dart";
 class libFacility {
   /*
    * CREATE
-   * -
-   * -
-   * -
-   * -
-   * -
-   * -
+   * - Create building
+   * - Create room in building
+   * - Create Organization Association
+   * - Create Communication Mechanism Association
    */
 
-   /*
-    * READ
-    * -
-    * -
-    * -
-    * -
-    * -
-    * -
-    */
+  /*
+   * READ
+   * - Find an existing Facilty of a given ID
+   * - Find all facilities meeting a given criterea
+   * - Find Organization Associations referencing a given Facility
+   * - Find Communication Mechanism Associations referencing a given Facility
+   * - Find detailed information of a given Facility (multipart)
+   */
 
-    /*
-     * UPDATE
-     * -
-     * -
-     * -
-     * -
-     * -
-     * -
-     */
+  /*
+   * UPDATE
+   * - Alter dates for a given Organization Association
+   * - Alter dates for a given Communication Mechanism Association
+   * - Terminate an Organization Association
+   * - Terminate a Communication Mechanism Association
+   * - Set the name of a given Facility
+   */
 
-     /*
-      * DELETE
-      * -
-      * -
-      * -
-      * -
-      * -
-      * -
-      */
+  /*
+   * DELETE
+   * - Delete a given Building
+   * - Delete a given Room
+   * - Delete an Organization Association
+   * - Delete a Communication Mechanism Association
+   */
 }
